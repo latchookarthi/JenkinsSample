236ea67b611ac981efac13bd1d8a1ec18a19096d
rajalakshmi.ragavan <rajalakshmi.ragavan@sample.com>
2018-10-17 13:25:20 -0400

    testng annotation removed

--- JenkinSample/src/main/java/jenkins/jenkins/App.java
+++ JenkinSample/src/main/java/jenkins/jenkins/App.java
@@ -2,8 +2,7 @@ package jenkins.jenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+
 
 
 /**
