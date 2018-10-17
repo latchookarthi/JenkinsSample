8b3efc0de933f8bfb3a8974585e8e52544d5d93c
rajalakshmi.ragavan <rajalakshmi.ragavan@sample.com>
2018-10-17 13:08:43 -0400

    removing abstract class

--- JenkinSample/src/main/java/jenkins/jenkins/App.java
+++ JenkinSample/src/main/java/jenkins/jenkins/App.java
@@ -5,17 +5,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+
 /**
  * Hello world!
  *
  */
-public abstract class App 
+public class App 
 
 {
-    protected WebDriver driver = null;
+     WebDriver driver = null;
 //	 protected static WebDriver driver;
 @BeforeTest
-    protected void resuse()
+     void resuse()
     {
 //	System.setProperty("webdriver.chrome.driver", "C:\\Users\\rajalakshmi.ragavan\\Downloads\\chromedriver_win32 (5)\\chromedriver.exe");	
 	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src.main.resources\\drivers\\chromedriver.exe");
@@ -27,7 +28,7 @@ public abstract class App
     }
 
     @AfterTest
-    protected void quit()
+    public void quit()
     {
     
 //System.setProperty("webdriver.chrome.driver", "C:\\Users\\rajalakshmi.ragavan\\Downloads\\chromedriver_win32 (5)\\chromedriver.exe");
