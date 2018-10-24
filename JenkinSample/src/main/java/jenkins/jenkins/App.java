a0bbd2837a5aa03a58ce4ae40357b3658b90bb20
Lakshmi <rajalakshmi.x.ragavan.-ND@disney.com>
2018-10-24 16:33:07 -0400

    capabilities

--- JenkinSample/src/main/java/jenkins/jenkins/App.java
+++ JenkinSample/src/main/java/jenkins/jenkins/App.java
@@ -27,9 +27,9 @@ static
 //    	 driver		=new SafariDriver();
 
 
-//    	 DesiredCapabilities capability = DesiredCapabilities.chrome();        
-//         capability.setCapability("ignoreZoomSetting", true);
-//         capability.setCapability("ignoreProtectedModeSettings", true);
+    	 DesiredCapabilities capability = DesiredCapabilities.chrome();        
+         capability.setCapability("ignoreZoomSetting", true);
+         capability.setCapability("ignoreProtectedModeSettings", true);
 ////System.setProperty("webdriver.chrome.driver", "C:\\Users\\rajalakshmi.ragavan\\Downloads\\chromedriver_win32 (5)\\chromedriver.exe");
 //    	 System.out.println("driver loaded");
 ////    	 System.out.println(System.getProperty("user.dir")+"\\src.main.resources\\drivers\\chromedriver.exe");
